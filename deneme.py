b678076fad91be8752dcb2006e7da8cfc70e01e8
Darkwolker <98810837+Darkwolker@users.noreply.github.com>
2022-02-10 22:35:37 +0300

    Add files via upload

---
+++ deneme.py
@@ -0,0 +1,23 @@
+print ("""
+##################################
+#  coder by darkwoler            #
+#  code injection tarama sistemi #
+##################################
+
+""")
+dork = input("lütfen dork giriniz : ")
+
+import time
+time.sleep(2)
+
+print ("arama baslatıldı lutfen bekleyınız !!! ")
+
+import time
+time.sleep(3)
+
+print ("girdiginiz dork taranıyor ")
+
+import time 
+time.sleep(5)
+
+print ("siteler.txt")
