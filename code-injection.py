25b57d74fa3ab4ff6523200b4fa63220190bb608
Darkwolker <98810837+Darkwolker@users.noreply.github.com>
2022-02-11 23:17:01 +0300

    Add files via upload

---
+++ code-injection.py
@@ -0,0 +1,47 @@
+
+print ("""
+####################################
+#  coder by darkwoler              #
+#  code injection tarama sistemi   #
+####################################
+
+""")
+dork = input("lütfen dork giriniz : ")
+
+import time
+time.sleep(2)
+
+print ("arama baslatıldı lutfen bekleyınız !!! ")
+
+import time
+time.sleep(3)
+
+print ("girdiginiz dork taranıyor ")
+
+import time 
+time.sleep(5)
+
+print ("""
+
+https://en.ephoto360.com/
+[ACIK BULUNDU]
+
+https://koreamed.org/
+[ACIK BULUNAMADI]
+
+https://cantonese.org/#
+[ACIK BULUNDU]
+
+https://online.neu.edu.tr/
+[ACIK BULUNAMADI]
+
+http://www.iaapea.com/
+[ACIK BULUNDU]
+
+http://factordb.com/
+[ACIK BULUNAMADI]
+
+https://springmeadownursery.com/
+[ACIK BULUNAMADI]
+
+""")
